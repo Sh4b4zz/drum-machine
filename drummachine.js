23c35600e078cecd94fb726b776d2910c8217c49
Sh4b4zz <59487217+Sh4b4zz@users.noreply.github.com>
2020-06-26 15:56:04 -0500

    Add files via upload

---
+++ drummachine.js
@@ -0,0 +1,53 @@
+var audio = new Audio("http://dight310.byu.edu/media/audio/FreeLoops.com/3/3/Free%20Drum%20Kick%2017-927-Free-Loops.com.mp3" ) ;
+audio.oncanplaythrough = function(){
+audio.currenttime = 0;
+audio.play();
+}
+
+var horn = new Audio("http://dight310.byu.edu/media/audio/FreeLoops.com/2/2/Cowbell%20Wav%20Sound-22317-Free-Loops.com.mp3" ) ;
+horn.oncanplaythrough = function(){
+Audio.currenttime = 0;
+horn.play();
+}
+
+var hat = new Audio("http://dight310.byu.edu/media/audio/FreeLoops.com/5/5/Lil%20John%20HiHat%203-2148-Free-Loops.com.mp3" ) ;
+hat.oncanplaythrough = function(){
+audio.currenttime = 0;
+hat.play();
+}
+
+var snare = new Audio("http://dight310.byu.edu/media/audio/FreeLoops.com/3/3/Free%20Drum%20Snare%208-960-Free-Loops.com.mp3" ) ;
+snare.oncanplaythrough = function(){
+audio.currenttime = 0;
+snare.play();
+}
+
+var deep = new Audio("http://dight310.byu.edu/media/audio/FreeLoops.com/1/1/909%20Crash%2001-5826-Free-Loops.com.mp3" ) ;
+deep.oncanplaythrough = function(){
+audio.currenttime = 0;
+deep.play();
+}
+
+var bell = new Audio("http://vincentbernay.free.fr/free-percussions-loops/sleigh%20bells.mp3" ) ;
+bell.oncanplaythrough = function(){
+audio.currenttime = 0;
+bell.play();
+}
+
+var tom1 = new Audio("http://www.randomthink.net/labs/html5drums/drumkit/Tom%20Low.mp3" ) ;
+tom1.oncanplaythrough = function(){
+audio.currenttime = 0;
+tom1.play();
+}
+
+var tom2 = new Audio("http://www.randomthink.net/labs/html5drums/drumkit/Tom%20Mid.mp3" ) ;
+tom2.oncanplaythrough = function(){
+audio.currenttime = 0;
+tom2.play();
+}
+
+var tom3 = new Audio("http://dight310.byu.edu/media/audio/FreeLoops.com/3/3/Drum%20Roll%20008-3036-Free-Loops.com.mp3" ) ;
+tom3.oncanplaythrough = function(){
+audio.currenttime = 0;
+tom3.play();
+}
